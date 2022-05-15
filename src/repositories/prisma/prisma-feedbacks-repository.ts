eb2550e55427872aab737b11e217334bfc3640b7
Teloschet <chrisaleiro@gmail.com>
2022-05-15 16:12:21 -0300

    feat!: create a class for prisma and implements feedbacks

---
+++ src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -0,0 +1,7 @@
+import { FeedbackCreateData, FeedbacksRepository } from "../feedbacks-repository";
+
+export class PrismaFeedbacksRepository implements FeedbacksRepository {
+  create(data: FeedbackCreateData) {
+    
+  }
+}
