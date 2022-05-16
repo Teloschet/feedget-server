6bfcb856803b03bce0e66cc5951e93f3bacebcc4
Teloschet <chrisaleiro@gmail.com>
2022-05-15 22:52:22 -0300

    fix!: create async function for create feedback

--- src/repositories/prisma/prisma-feedbacks-repository.ts
+++ src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -1,7 +1,14 @@
+import { prisma } from "../../prisma";
 import { FeedbackCreateData, FeedbacksRepository } from "../feedbacks-repository";
 
 export class PrismaFeedbacksRepository implements FeedbacksRepository {
-  create(data: FeedbackCreateData) {
-    
+  async create({ type, comment, screenshot }: FeedbackCreateData) {
+    await prisma.feedback.create({
+      data: {
+        type,
+        comment,
+        screenshot,
+      }
+    })
   }
 }
